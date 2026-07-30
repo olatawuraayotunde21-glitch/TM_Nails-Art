0980bf97c63775725743f4ba1e9189c7d6ba67da
Teemax <Timileyinayotunde@gmail.com>
2026-07-30 16:54:15 +0100

    Switch to non-manifest Whitenoise storage to fix admin icon build error

--- config/settings.py
+++ config/settings.py
@@ -141,9 +141,8 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
-WHITENOISE_MANIFEST_STRICT = False
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
